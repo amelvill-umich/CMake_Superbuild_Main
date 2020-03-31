37f49e99c7db2bab4686130c43b970a6362193f3
ajm <ajm>
2020-03-31 14:52:43 -0400

    for debugging purposes the find script now prints out all its variables

--- CMake/FindSUPERBUILD_EXTERN.cmake
+++ CMake/FindSUPERBUILD_EXTERN.cmake
@@ -2,3 +2,9 @@ message("Start of find script for the extern project.")
 find_program(EXTERN_BINARY "extern_binary")
 
 message("EXTERN_BINARY IS " ${EXTERN_BINARY})
+
+get_cmake_property(_variableNames VARIABLES)
+list (SORT _variableNames)
+foreach (_variableName ${_variableNames})
+    message(STATUS "${_variableName}=${${_variableName}}")
+endforeach()
