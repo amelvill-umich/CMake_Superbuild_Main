b097ae20710f6d981019b31e65fe3c1832829734
ajm <ajm>
2020-03-31 14:55:09 -0400

    The find script now uses the superbuild extern cache variable as a hint

--- CMake/FindSUPERBUILD_EXTERN.cmake
+++ CMake/FindSUPERBUILD_EXTERN.cmake
@@ -1,10 +1,16 @@
 message("Start of find script for the extern project.")
-find_program(EXTERN_BINARY "extern_binary")
+
+# note that SUPERBUILD_EXTERN_DIR is a cache varaible for this project
+find_program(
+                EXTERN_BINARY 
+                "extern_binary"
+                ${SUPERBUILD_EXTERN_DIR}]
+            )
 
 message("EXTERN_BINARY IS " ${EXTERN_BINARY})
 
-get_cmake_property(_variableNames VARIABLES)
-list (SORT _variableNames)
-foreach (_variableName ${_variableNames})
-    message(STATUS "${_variableName}=${${_variableName}}")
-endforeach()
+# get_cmake_property(_variableNames VARIABLES)
+# list (SORT _variableNames)
+# foreach (_variableName ${_variableNames})
+#     message(STATUS "${_variableName}=${${_variableName}}")
+# endforeach()
