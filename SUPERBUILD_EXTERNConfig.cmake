cc61dc492c69bc6772f21c135c5c8cd9d9adeb29
ajm <ajm>
2020-04-01 10:07:49 -0400

    Deleted config file, I don't think Main should have this

--- SUPERBUILD_EXTERNConfig.cmake
+++
@@ -1,7 +0,0 @@
-# does Main execute the config file???
-message("Hello from SUPERBUILD_EXTERNConfig.cmake!")
-install(EXPORT DepTargets
-        FILE "DepTargets.cmake" # note: this file gets generated
-        NAMESPACE Extern::
-        DESTINATION "DirectoryForCmakeSuperbuild_Extern/cmake/Extern"
-)
